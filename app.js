5590942aed1e6a50e7c690eb57a5be1c413be121
sparshhh <45496287+sparshhh@users.noreply.github.com>
2021-05-10 11:42:07 +0530

    10may

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("output");
 
 console.log(txtInput)
+
+console.log(outputDiv)
 
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
     console.log("clicked!")
     console.log("input" , txtInput.value)
 })
+
